c921f743d1db4cba4571d561d83b3eaada2190f2
stredes <gianlucassanmartin@gmail.com>
2025-10-22 21:44:50 -0300

    creacion servicios + integracion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Calculadora from './components/calculadoraintegral';
 import NavbarPropio from './components/Navbar';
 import Heropropio from './components/Hero';
+import ServiciosPropios from './components/Servicios';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className='container'>
         <NavbarPropio/>
         <Heropropio/>
+        <ServiciosPropios/>
         <Calculadora/>
           
         </div>
