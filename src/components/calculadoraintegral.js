a5c2ec795c7a90d64a105033b4ea51479595996c
PAAWSA02LC0923 <Duoc@PAAWSA02LC0923.duoc.acad>
2025-10-22 22:14:20 -0300

    Cambios extraños

--- src/components/calculadoraintegral.js
+++ src/components/calculadoraintegral.js
@@ -5,60 +5,90 @@ import { useState } from 'react';
 
 function Calculadora() {
 
-    const [potenciaDePanel, setPotenciaDePanel] = useState(0);
+    const [potenciaDePanel, setPotenciaDePanel] = useState("");
+    const [cantidadDePaneles, setCantidadDePaneles] = useState("");
+    const [inversor, setInversor] = useState("");
+    const [bateriaPrecio, setBateriaPrecio] = useState("");
+    const [cantidadBaterias, setCantidadBaterias] = useState("");
+    const [estructuraCableado, setEstructuraCableado] = useState("");
 
 
+    console.log(potenciaDePanel);
 
 
 
     return (
 
 
-        <div className='row mt-3'>
+        <div>
+            <div className='row mt-3'>
                 <div className='col-lg-3'>
-                    <h4>Potencia de Panel (W)</h4>
-                    <input id='potenciaDePanel' name='potenciaDePanel' placeholder='450' type='number' className='form-control' value={potenciaDePanel} onChange={(e) => setPotenciaDePanel(e.target.value)}></input>
+                    <div className='form-group mt-3'>
+                        <h4>Potencia de Panel (W)</h4>
+                        <input id='potenciaDePanel' name='potenciaDePanel' placeholder='450' type='number' className='form-control' value={potenciaDePanel} onChange={(e) => setPotenciaDePanel(parseInt(e.target.value))}></input>
+                    </div>
+                    <div className='form-group mt-3'>
+                        <h4>Inversor (precio)</h4>
+                        <input id='inversor' name='inversor' placeholder='650000' type='number' className='form-control' value={inversor} onChange={(e) => setInversor(parseInt(e.target.value))}></input>
+                    </div>
+                    <div className='form-group mt-3'>
+                        <h4>Cantidad baterías</h4>
+                        <input id='cantidadBaterias' name='cantidadBaterias' placeholder='1' type='number' className='form-control' value={cantidadBaterias} onChange={(e) => setCantidadBaterias(parseInt(e.target.value))}></input>
+                    </div>
                 </div>
+
                 <div className='col-lg-3'>
-                    <h4>Cantidad de Paneles</h4>
-                    <input id='potenciaDePanel' name='potenciaDePanel' placeholder='8' type='number' className='form-control' value={potenciaDePanel} onChange={(e) => setPotenciaDePanel(e.target.value)}></input>
+                    <div className='form-group mt-3'>
+                        <h4>Cantidad de Paneles</h4>
+                        <input id='cantidadDePaneles' name='cantidadDePaneles' placeholder='8' type='number' className='form-control' value={cantidadDePaneles} onChange={(e) => setCantidadDePaneles(parseInt(e.target.value))}></input>
+                    </div>
+                    <div className='form-group mt-3'>
+                        <h4>Batería (precio unidad)</h4>
+                        <input id='bateriaPrecio' name='bateriaPrecio' placeholder='320000' type='number' className='form-control' value={bateriaPrecio} onChange={(e) => setBateriaPrecio(parseInt(e.target.value))}></input>
+                    </div>
+                    <div className='form-group mt-3'>
+                        <h4>Estruct./Cableado</h4>
+                        <input id='estructuraCableado' name='estructuraCableado' placeholder='180000' type='number' className='form-control' value={estructuraCableado} onChange={(e) => setEstructuraCableado(parseInt(e.target.value))}></input>
+                    </div>
                 </div>
 
+                <br />
 
-                <div className='w-100'></div>  /* Esta linea hace un salto de linea en bootstrap */ 
 
 
-            <div className='col-lg-6'>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td colSpan={2}>Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
+                <div className='col-lg-6'>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan={2}>Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
+
         </div>
     );
 
--- src/components/codigoborrador.js
+++ src/components/codigoborrador.js
@@ -1,4 +1,3 @@
-  const [cantidadDePaneles, setCantidadDePanele] = useState(0);
   const [inversor, setInversor] = useState(0);
     const [bateriaPrecio, setBateriaPrecio] = useState(0);
     const [cantidadBaterias, setCantidadBaterias] = useState(0);
@@ -8,3 +7,123 @@
 
 
     // Como poner varias filas sin poner otra row
+
+//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+import React from 'react';
+import { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+
+function CotizadorPrincipal() {
+
+    const [precioCpu, setPrecioCpu] = useState(0);
+
+    const [precioGpu, setPrecioGpu] = useState(0);
+    const [cantRam, setCantRam] = useState(0);
+    const [cantDescuento, setCantDescuento] = useState(0);
+    const [tipoGarantia, setTipoGarantia] = useState("");
+
+
+    //const Total = precioCpu + precioGpu + cantRam - cantDescuento;
+    const adicionalGarantia = tipoGarantia == 1 ? 0 : 100000;
+
+    const subTotal = parseInt(precioCpu) + parseInt(precioGpu) + parseInt(cantRam);
+    const totalIva = parseInt(subTotal)*0.19;
+    const Total = parseInt(subTotal) + parseInt(totalIva) - parseInt(cantDescuento) + parseInt(adicionalGarantia);
+   
+
+    return (
+        <div>
+            <div className='row'>
+                <div className='col-lg-12 text-center bg-secondary text-white p-3 rounded'>
+                    <h2>DEMO:</h2>
+                    <h4>Cotizador de Componentes</h4>
+                </div>
+            </div>
+
+            <div className='row mt-3'>
+                <div className='col-lg-6'>
+                   
+
+                    <label className='form-label' htmlFor='precioCpu'>Precio CPU</label>
+                    <input id='precioCpu' name='precioCpu' placeholder='1000000' type='number' className='form-control' value={precioCpu} onChange={(e) => setPrecioCpu(e.target.value)}></input>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='precioGpu'>Precio GPU (por módulo)</label>
+                        <input type='number' id='precioGpu' name='precioGpu' className='form-control' value={precioGpu} placeholder='250000' onChange={(e) => setPrecioGpu(e.target.value)}></input>
+                    </div>
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='cantRam'>Cantidad módulos RAM (por módulo)</label>
+                        <input type='number' id='cantRam' name='cantRam' className='form-control' value={cantRam} placeholder='250000' onChange={(e) => setCantRam(e.target.value)}></input>
+                    </div>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='tipoGarantia'>Garantía</label>
+                        <select className='form-select' id='tipoGarantia' name='tipoGarantia' value={tipoGarantia} onChange={(e) => setTipoGarantia(e.target.value)}>
+                            <option>Seleccione garantía</option>
+                            <option value={1}>Normal</option>
+                            <option value={2}>Extendida</option>
+                        </select>
+                    </div>
+                   
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='cantDescuento'>Descuento</label>
+                        <input type='number' id='cantDescuento' name='cantDescuento' className='form-control' value={cantDescuento} placeholder='250000' onChange={(e) => setCantDescuento(e.target.value)}></input>
+                    </div>
+
+                    <div className='form-group mt-3'>
+                        <a className='btn btn-warning' onClick={(e) => {
+                           
+                           setPrecioCpu(0);
+                            setCantRam(0);
+                            setCantDescuento(0);
+                            setPrecioGpu(0);
+                            setTipoGarantia("");
+
+                        } } ><i class="fa-solid fa-poo"></i> Limpiar </a>
+                    </div>
+
+                </div>
+
+                <div className='col-lg-6'>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Concepto</th>
+                                <th>Monto</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Subtotal</td>
+                                <td>$ {parseInt(subTotal).toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>IVA (19%)</td>
+                                <td>$ {totalIva.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Descuento </td>
+                                <td>$ {cantDescuento.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Total</td>
+                                <td>$ {Total.toLocaleString()}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default CotizadorPrincipal;
